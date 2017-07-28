873b38b80548ab5fefe9a3e6740d4dca0da1bcab
carcigenicate <slomifier@gmail.com>
2017-07-27 18:25:05 -0700

    Need to figure out if I want to store x/y coords and take up more space,  or use just a index and need to pass in the width so the math can be done. Using an index will also be (very) slightly more computationally intensive since constant multiplication will need to be done to compute the x/y.

--- src/picture_ai/gene_parser.clj
+++ src/picture_ai/gene_parser.clj
@@ -2,8 +2,10 @@
 
 (defrecord Circle [position radius])
 
+; TODO: Store [x y radius] triplets in the genes, or [i radius] pairs?
+
 (defn split-into-circles [genes]
   (partition 2 genes))
 
 (defn parse-split-into-circles [split-genes]
-  (for [[]]))
+  (for [[x y r] split-genes]))
